402ff03786e1c7cd268c2fb8027b68e5ea3e0d61
devitadwiindayani <devitadwiindayani@gmail.com>
2026-04-10 10:09:17 +0700

    membuat method koneksi database

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -1,20 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=LAPTOP-GO2648H1\\DEVITADWI;Initial Catalog =DBAkademikADO;Integrated Security=True";
+
         public Form1()
         {
             InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+        private void ConnectDatabase()
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                MessageBox.Show("Koneksi berhasil");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal: " + ex.Message);
+            }
+        }
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            ConnectDatabase();
         }
 
         private void txtNama_TextChanged(object sender, EventArgs e)
@@ -36,5 +54,11 @@ namespace CRUDMahasiswaADO
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
