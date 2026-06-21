2e4cce672d33cc0223e31c6ce478d514666f7092
devitadwiindayani <devitadwiindayani@gmail.com>
2026-06-21 19:10:52 +0700

    Menambahkan Form 3

---
+++ CRUDMahasiswaADO/Form3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+
+    public partial class Form3 : Form
+    {
+        static string connectionString = "Data Source=LAPTOP-GO2648H1\\DEVITADWI;Initial Catalog=DBAkademikADO;User ID=sa;Password=PasswordSA";
+
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+
+        ListMahasiswa listMahasiswa = new ListMahasiswa();
+
+        string prodi { get; set; }
+        DateTime tglMasuk { get; set; }
+
+        public Form3(string Prodi, DateTime TglMasuk)
+        {
+            InitializeComponent();
+
+            prodi = Prodi;
+            tglMasuk = TglMasuk;
+
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@inProdi", prodi);
+                cmd.Parameters.AddWithValue("@inTglMasuk", tglMasuk.Year);
+
+                da = new SqlDataAdapter(cmd);
+
+                dtMahasiswa = new DataTable();
+                da.Fill(dtMahasiswa);
+
+                conn.Close();
+
+                listMahasiswa.SetDataSource(dtMahasiswa);
+
+                crystalReportViewer1.ReportSource = listMahasiswa;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data : " + ex.Message);
+            }
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
